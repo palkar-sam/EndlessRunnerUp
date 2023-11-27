9e0bd31ace4c8643e51557e1a27c3b3a58d5d611
Sameer <palkar.sam@gmail.com>
2023-11-27 12:01:50 +0530

    Logs files are added for debugging the game.

---
+++ Assets/Scripts/DebugLogs/RunnerLog.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class RunnerLog : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI highScoreText;
+
+    private static RunnerLog _instance;
+    private static string _logStr = string.Empty;
+
+    private bool _isShown;
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    public static void Log(string str)
+	{
+		Debug.Log(str);
+        _logStr = string.Concat(_logStr, "\n", str);
+
+        if(_instance != null)
+            _instance.ShowLogs();
+    }
+
+    public void ShowLogPanel()
+    {
+        _isShown = !_isShown;
+        highScoreText.gameObject.SetActive(_isShown);
+        ShowLogs();
+    }
+
+    private void ShowLogs()
+    {
+        if(highScoreText.gameObject.activeInHierarchy)
+            highScoreText.text = _logStr;
+    }    
+}
+
